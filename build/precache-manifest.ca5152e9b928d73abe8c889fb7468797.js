3677c8c07a594d69a0d646ae8739677f66a54333
rased <sumchmohammadtarek@Sumchs-iMac.local>
2020-09-30 13:13:16 +0600

    first commit

---
+++ build/precache-manifest.ca5152e9b928d73abe8c889fb7468797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb819b07f2341194a1c4aa232725677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f534ce5694b48fd654b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f534ce5694b48fd654b",
+    "url": "/static/js/main.3dd8d385.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi fugit doloremque quos explicabo repellendus iste vel, ut, exercitationem odio atque pariatur consectetur culpa temporibus necessitatibus, facilis repellat beatae nesciunt veritatis.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
